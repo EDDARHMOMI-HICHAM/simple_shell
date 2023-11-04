0d14119450420f3517617a356107219c54729510
Dennis Adigwe <adigwedennis@gmail.com>
2023-11-04 14:25:03 +0100

    created shell.h

---
+++ shell.h
@@ -0,0 +1,25 @@
+#ifndef SHELL_H
+#define SHELL_H
+
+#include <stddef.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <dirent.h>
+#include <stdio.h>
+#include <string.h>
+#include <signal.h>
+#include <sys/stat.h>
+#include <sys/time.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/resource.h>
+#include <errno.h>
+
+int _putchar(char c);
+char *read_line(void);
+char **tokenize_cmd(char *cmd);
+
+
+#endif
+
