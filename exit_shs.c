cbba2f0727a3b821b77a75daee7db43418138c35
Hix.Mox <heddarhmoumigmail.com>
2023-11-15 17:27:31 +0100

    push 2end edit to exit

--- exit_shs.c
+++ exit_shs.c
@@ -1,10 +1,10 @@
 #include "shell.h"
 
-int exit_shs(char *cmd)
-{
+int exit_shs(char *cmd){
 	char **tokens;
 	int status;
 	char *full_path;
+	int exit_status = 0;
 	
 	if (cmd == NULL || cmd[0] == '\0')
 		return (0);
@@ -12,20 +12,23 @@ int exit_shs(char *cmd)
 	tokens = tokenize_cmd(cmd);
 	full_path = search_path(tokens[0]);
 	
-	if (strcmp(tokens[0], "exit") == 0) 
+	if (strcmp(tokens[0], "exit") == 0)
 	{
+		if (tokens[1])
+			exit_status = atoi(tokens[1]);
+	
 		free(tokens);
-		exit(EXIT_SUCCESS);
+		exit(exit_status);
 	}
 	else
 	{
 		full_path = search_path(tokens[0]);
 		status = exec_cmd(tokens, full_path);
 		if (status != 0)
-
 			printf("Error executing command.\n");
-		
+
 		free(tokens);
 	}
+	
 	return (0);
 }
