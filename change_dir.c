3c0e298d96ccb74da3afd39baf01eec8464ae050
Hix.Mox <heddarhmoumigmail.com>
2023-11-15 04:15:07 +0100

    handle cd whithout condition

--- change_dir.c
+++ change_dir.c
@@ -9,20 +9,24 @@
 
 int change_dir(char *cmd)
 {
-	if (cmd == NULL)
+	if (cmd == NULL || strcmp(cmd, "~") == 0)
 	{
-		printf("Usage: cd <directory>\n");
-		return (1);
+		if (chdir(getenv("HOME")) != 0)
+		{
+			printf("Failed to change directory to home.\n");
+			return (1);
+		}
+		return (0);
 	}
 
-	if (access(cmd, F_OK) != 0)
+	else if (access(cmd, F_OK) != 0)
 	{
 		printf("Directory does not exist.\n");
 		return (1);
 
 	}
 
-	if (chdir(cmd) != 0)
+	else if (chdir(cmd) != 0)
 	{
 		printf("Failed to change directory.\n");
 		return (1);
